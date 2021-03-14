d5f7e1537771d45f3fd59811598e760bc00b241b
oak1331 <oaks1331@gmail.com>
2021-03-14 19:26:43 +0900

    モデルファイルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to [:category, :condition, :delivery, :prefecture, :shipping_date]
+
 end
