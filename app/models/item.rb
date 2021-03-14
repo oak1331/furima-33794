e399f781a484373284929270b50763a6015e44bd
oak1331 <oaks1331@gmail.com>
2021-03-14 20:36:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,25 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to [:category, :condition, :delivery, :prefecture, :shipping_date]
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validares :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 
 end
