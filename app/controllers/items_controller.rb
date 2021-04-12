43696614ea74a9fae2362e40804d0ed83b503921
oak1331 <oaks1331@gmail.com>
2021-04-12 21:55:43 +0900

    商品購入機能の実装_修正_4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  before_action :sold_out_item, only: [:edit, :update]
+  before_action :sold_out_item, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
